a77e6bcb9a226fab06c6f0fc20a58f1d9a349d67
Tabardel <bastientabardel@outlook.com>
2020-09-29 15:12:33 +0200

    ajour heure

--- script.js
+++ script.js
@@ -22,4 +22,17 @@ function ajouter_ligne(heure, groupe, salle, nom){
         newCell.appendChild(newText);
     }
 }
-ajouter_ligne("10:15", "1A3B", "006", "TD EE")
+function afficher_heure(){
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    /*
+    Ce qui suit transforme les heures et les minutes en nombre a 2 chiffres(avec un zero devant en gros si c'est <10)*/
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    var time = h + ":" + m;
+    document.getElementById("horloge").innerText = time;
+    document.getElementById("horloge").textContent = time;
+    setTimeout(afficher_heure, 1000);//on attend 1 seconde, ouai c'est peut etre overkill mais balek
+}
+afficher_heure();//on est obliger de le lancer une fois pour initialiser
